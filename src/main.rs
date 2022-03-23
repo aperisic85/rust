5e7d24e2b0e9ff1a77e25641da0502153203b163
ap_smart_st <perisic.ante@gmail.com>
2022-03-23 12:49:54 +0100

    added render function

--- src/main.rs
+++ src/main.rs
@@ -43,11 +43,65 @@ fn parse_complex (s: &str) -> Option<Complex<f64>> {
 
 }
 
+/**
+D - upper left
+|---------|
+|         |
+|         |
+|---------| B - lower right
+ **/
+
+/// given row,col of a pixel in the output image, returning the
+/// corresponding point on complex plane.
+///
+/// 'bounds' is a pair giving the width and height of the image in pixels
+/// 'pixel' is a (column, row) pair indicating a particular pixel in that image
+/// on the complex plain designating the area our image covers.
+fn pixel_to_point(  bounds: (usize, usize),
+                    pixel: (usize,usize),
+                    upper_left: Complex<f64>,
+                    lower_right: Complex<f64> )
+    -> Complex<f64> {
+    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        /// subtraction (down) : pixel.1 increases as we go down,
+        /// but imaginary component increases as we go up
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+/// Render a rectangle of Mandelbrot set into a buffer of pixels
+fn render(  pixels: &mut [u8],
+            bounds: (usize, usize),
+            upper_left: Complex<f64>,
+            lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bound.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+
+}
+
+
 fn main() {
     
     escape_time(Complex { re: 0.30, im: 0.1 }, 20);
+    let my_complex = Complex::new(1,2);
+    let another = my_complex.scale(3);
+
+    println!("original: {} , scaled: {}", my_complex, another );
 }
-//tst
+
 #[test]
 fn test_parse_pair(){
     assert_eq!(parse_pair::<i32>("", ','), None);
@@ -60,4 +114,11 @@ fn test_parse_pair(){
     assert_eq!(parse_complex(",-2.5"), None);
 
 }
+
+#[test]
+fn test_pixel_to_point(){
+    assert_eq!(pixel_to_point((100,200),(25,175),Complex{re: -1.0, im: 1.0},Complex{re: 1.0, im: -1.0}),
+               Complex{ re: -0.5, im : -0.75});
+}
+
 
